5d3ff921ba83bcc93bff87702f5c17128ad4fde8
fellypsantos <fellypsantos2011@gmail.com>
2023-04-18 10:28:00 -0300

    first authenticated route

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('me', [AuthController::class, 'getMe']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
