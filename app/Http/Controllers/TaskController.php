ef7a34818dc3c925dae9df5713b86e86e50c9f53
fellypsantos <fellypsantos2011@gmail.com>
2023-04-18 23:35:54 -0300

    handle incoming requests from route

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskStoreUpdateValidation;
+use App\Services\TaskService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
+class TaskController extends Controller
+{
+
+    protected $taskService;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
+    public function index()
+    {
+        $tasks = $this->taskService->getAll();
+
+        return response()->json($tasks);
+    }
+
+    public function store(TaskStoreUpdateValidation $request)
+    {
+        $data = $request->validated();
+
+        try {
+            $task = $this->taskService->createNewTask($data);
+
+            return response()->json($task, Response::HTTP_CREATED);
+        } catch (ValidationException $ex) {
+            return response()->json(['message' => $ex->response], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    public function show(int $id)
+    {
+        try {
+            $task = $this->taskService->getTask($id);
+
+            return response()->json($task);
+        } catch (ModelNotFoundException $ex) {
+            return response()->json(['message' => $ex->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function update(TaskStoreUpdateValidation $request, int $id)
+    {
+        $data = $request->validated();
+
+        try {
+            $task = $this->taskService->updateTask($data, $id);
+
+            return response()->json($task);
+        } catch (ModelNotFoundException $ex) {
+            return response()->json(['message' => $ex->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function destroy(int $id)
+    {
+        try {
+            $result = $this->taskService->deleteTask($id);
+
+            return response()->json(['success' => $result], Response::HTTP_NO_CONTENT);
+        } catch (ModelNotFoundException $ex) {
+            return response()->json(['message' => $ex->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+}
