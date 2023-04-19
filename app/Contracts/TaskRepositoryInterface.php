e533920aa89a097f1f28330e8c08225dfe25537d
fellypsantos <fellypsantos2011@gmail.com>
2023-04-19 15:46:20 -0300

    fix interface to reflect changes in User params

--- app/Contracts/TaskRepositoryInterface.php
+++ app/Contracts/TaskRepositoryInterface.php
@@ -10,13 +10,13 @@ interface TaskRepositoryInterface
 {
     public function getAll(): Collection;
 
-    public function findTaskById(int $id, User $user): ?Task;
+    public function findTaskById(int $id): ?Task;
 
-    public function findTaskByTitle(string $title, User $user): ?Task;
+    public function findTaskByTitle(string $title): ?Task;
 
     public function createNewTask(array $data, User $user): Task;
 
-    public function updateTaskInformation(array $updated_data, int $id, User $user);
+    public function updateTaskInformation(array $updated_data, int $id);
 
-    public function deleteTask(int $id, User $user): bool;
+    public function deleteTask(int $id): bool;
 }
