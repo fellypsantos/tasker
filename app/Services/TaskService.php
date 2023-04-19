9d13328c3a3f7e447948071e027e4ab02d31a1f8
fellypsantos <fellypsantos2011@gmail.com>
2023-04-18 23:39:46 -0300

    service with business rules for each action

---
+++ app/Services/TaskService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\TaskRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+
+class TaskService
+{
+    protected $taskRepository;
+
+    public function __construct(TaskRepository $taskRepository)
+    {
+        $this->taskRepository = $taskRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->taskRepository->getAll();
+    }
+
+    public function createNewTask(array $data)
+    {
+        $user = auth()->user();
+
+        $task = $this->taskRepository->findTaskByTitle($data['title']);
+
+        if ($task) {
+            throw new ValidationException(null, 'Task with same title already exists.');
+        }
+
+        return $this->taskRepository->createNewTask($data, $user);
+    }
+
+    public function getTask(int $id)
+    {
+        $task = $this->taskRepository->findTaskById($id);
+
+        if (!$task) throw new ModelNotFoundException('Task not found.');
+
+        return $task;
+    }
+
+    public function updateTask(array $data, int $id)
+    {
+        $task = $this->taskRepository->findTaskById($id);
+
+        if (!$task) throw new ModelNotFoundException('Task not found.');
+
+        $task_updated = $this->taskRepository->updateTaskInformation($data, $id);
+
+        return $task_updated;
+    }
+
+    public function deleteTask(int $id)
+    {
+        $task = $this->taskRepository->findTaskById($id);
+
+        if (!$task) throw new ModelNotFoundException('Task not found.');
+
+        $result = $task->delete();
+
+        return $result;
+    }
+}
