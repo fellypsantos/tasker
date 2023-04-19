2ca9e8785ec6fa878c7c2b08698bdcac272b3ae0
fellypsantos <fellypsantos2011@gmail.com>
2023-04-18 23:36:44 -0300

    repository implementing the contract

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\TaskRepositoryInterface;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+class TaskRepository implements TaskRepositoryInterface
+{
+    public function getAll(): Collection
+    {
+        return Task::all();
+    }
+
+    public function findTaskById(int $id): ?Task
+    {
+        return Task::find($id);
+    }
+
+    public function findTaskByTitle(string $title): ?Task
+    {
+        $task = Task::where('title', $title)->first();
+
+        return $task;
+    }
+
+    public function createNewTask(array $data, User $user): Task
+    {
+        $task = new Task($data);
+
+        $task->user_id = $user->id;
+
+        $task->save();
+
+        return $task;
+    }
+
+    public function updateTaskInformation(array $updated_data, int $id): Task
+    {
+        $task = Task::findOrFail($id);
+
+        $task->update($updated_data);
+
+        return $task;
+    }
+
+    public function deleteTask(int $id): bool
+    {
+        $task = Task::findOrFail($id);
+
+        $result = $task->delete();
+
+        return $result;
+    }
+}
