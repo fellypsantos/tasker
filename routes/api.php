ce13944710e133b512161693f55ca8e21eb920dc
fellypsantos <fellypsantos2011@gmail.com>
2023-04-18 23:41:28 -0300

    routes to manage tasks

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,4 +10,6 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('me', [AuthController::class, 'getMe']);
+
+    Route::apiResource('tasks', TaskController::class);
 });
